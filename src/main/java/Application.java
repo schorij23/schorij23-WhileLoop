5365bb2b1cc8dd7edc0080a09e8e634032b331f1
schorij23 <j_schori@aol.com>
2023-02-22 01:16:03 +0000

    gfsafgsgfdfds

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,7 +4,8 @@ public class Application {
      * No changes you make here affect the test case results. You must still run & pass the test cases.
      * All implementation code should be written in the Lab class.
      *
-     * The Lab.whileExample lab will get the sum of all numbers from 1 to some number >1. All code required to
+     *hdghg
+     *  The Lab.whileExample lab will get the sum of all numbers from 1 to some number >1. All code required to
      * calculate this is provided in the while loop lab documentation.
      */
     public static void main(String[] args) {
